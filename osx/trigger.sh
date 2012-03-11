de32cb3746ab2cd947cea49bd9cb09d436c915b4
Lang Sharpe <langer8191@gmail.com>
2012-03-11 16:07:04 +1000

    Parse command-line agruments and clean up temp files.

--- osx/trigger.sh
+++ osx/trigger.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# Trigger: Run a command when a change occurs on the filesystem
+# Usage:
+#       trigger [<paths>] -- <command>
 
-WATCH_DIR=~/Dropbox/Projects/trigger/test
-TRIGGER_CMD="echo 'TRIGGERED'"
+# Parse command line.
+WATCH_DIRS=""
+TRIGGER_CMD=""
 
-# Create a temporary directory to put the named pipe in
+# Loop through options until we reach "--", or the end
+while [ "$1" != "--" -a $# -ne 0 ]; do
+        WATCH_DIRS="$WATCH_DIRS $1"
+        shift
+done
+
+# shift out the "--" option. The rest is the commend to run
+shift
+TRIGGER_CMD=$*
+
+# Temporary directory to put a named pipe in
 TEMP_DIR=`mktemp -d -t trigger`
 
 # Named pipe to read/write filesystem events
@@ -12,12 +26,30 @@ mkfifo $FSEVENT_PIPE
 
 # Write file system events into the pipe
 FSEVENT_WATCH_CMD="$(dirname $0)/bin/fsevent_watch -f classic"
-$FSEVENT_WATCH_CMD $WATCH_DIR 1> $FSEVENT_PIPE 2> /dev/null &
+$FSEVENT_WATCH_CMD $WATCH_DIRS 1> $FSEVENT_PIPE 2> /dev/null &
+
+# Register a signal handler for if (when) we are told to exit
+cleanup()
+# Remove temporary files
+{
+        echo "CTRLC"
+        if [ -p "$FSEVENT_PIPE" ]
+        then
+                rm "$FSEVENT_PIPE"
+        fi
+
+        if [ -d "$TEMP_DIR" ]
+        then
+                rmdir "$TEMP_DIR"
+        fi
+        exit $?
+}
+trap cleanup SIGINT
 
 # Read from the pipe, and run the trigger command for every line read
 cat $FSEVENT_PIPE | while read ; do
         $TRIGGER_CMD
 done
 
-
-
+# If we manage to get here without an interupt, then cleanup
+cleanup
